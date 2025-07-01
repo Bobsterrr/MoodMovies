f7d5ebfcd697b332b1ee90912fa9eba584102e8a
bobsterrr <lobanovdaniilll@gmail.com>
2025-07-01 11:00:27 +0200

    placeholder is added

--- likeness.js
+++ likeness.js
@@ -1,5 +1,5 @@
 export const likeContainer = document.getElementById('like_container');
-export const dislikeContainer = document.getElementById('dislike_container')
+export const dislikeContainer = document.getElementById('dislike_container');
 export const reset = document.getElementById('reset');
 export const like = document.getElementById('like');
 export const dislike = document.getElementById('dislike');
---
+++ modules/listCreating.js
@@ -0,0 +1,19 @@
+class ListCreating extends HTMLElement {
+	constructor() {
+		super();
+	}
+	connectedCallback() {
+		const shadow = this.attachShadow({mode: 'open'});
+
+		const openList = document.createElement('button');
+		openList.setAttribute('id', 'openList');
+
+		const tableList = document.createElement('ul');
+		tableList.setAttribute('id','tableList')
+
+		const dialog = document.createElement('dialog');
+		dialog.setAttribute
+	}
+}
+
+customElements.define('list-creating', ListCreating);
